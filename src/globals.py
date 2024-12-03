bcecc48f371d691e41dcd383a92914486d8a6799
Joeywysocki <118062943+Joeywysocki@users.noreply.github.com>
2024-12-02 20:35:37 -0500

    Updates to requirements and final report

--- src/globals.py
+++ src/globals.py
@@ -8,5 +8,8 @@ class LICENSE_PLATE_GLOBALS:
     SAVE_HTML = True
     SAVE_PNG = True
     USE_COMET = True  # Set to True to use Comet.ml. This adds additional logging capability and visualization tools. Note you must have a comet account and API key to use this feature
-    COMET_CONFIG = path.join(getenv('HOME'))
+    home_dir = getenv('HOME') or getenv('USERPROFILE')
+    if not home_dir:
+        raise EnvironmentError("Home directory environment variable ('HOME' or 'USERPROFILE') is not set.")
+    COMET_CONFIG = path.join(home_dir)
     PROJECT_NAME = "license-plate-detection"
